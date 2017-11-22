0981b4b8dd8850f59f028b14076c1acc5d788df6
giannizatt <gianni.zattoni@intesasanpaolo.com>
2017-11-22 22:04:14 +0100

    go on

--- NCFApp.py
+++ NCFApp.py
@@ -22,3 +22,36 @@ df_totbdt['MESE'] = df_sstor['MESE']
 df_totbdt['FL_BDT'] = df_sstor['FL_IMP']+df_sstor['FL_PER']+df_sstor['FL_AZR']+df_sstor['FL_RET']
 df_totbdt['CP_BDT'] = df_sstor['CP_IMP']+df_sstor['CP_PER']+df_sstor['CP_AZR']+df_sstor['CP_RET']
 df_totbdt['ST_BDT'] = df_sstor['ST_IMP']+df_sstor['ST_PER']+df_sstor['ST_AZR']+df_sstor['ST_RET']
+
+# Funzione che espone l'ultimo consuntivo caricato
+def Lastcons():
+    df_mesi = df_sstor['MESE']
+    return max(df_mesi)
+
+# Funzione che prepara un grafico e lo salva in una figura
+def SimplePlot(SeriePandas=None,LabelofSerie=None):
+    plt.plot(SeriePandas,label=LabelofSerie)
+    plt.legend()
+    return plt.figure()
+
+
+# Frame principale
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.quitButton.grid()
+        
+    def quit(self):
+        self.destroy()
+        return 
+
+app = Application()
+app.master.title('New Customers Forecast Application - rel. 1.0 - (C) Gianni Zattoni')
+app.mainloop()
+
