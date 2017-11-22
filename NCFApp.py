c57686e6317a9fb357fab401e8d1509e0a1498ee
giannizatt <gianni.zattoni@intesasanpaolo.com>
2017-11-22 21:21:32 +0100

    and so on

--- NCFApp.py
+++ NCFApp.py
@@ -13,5 +13,12 @@ import Tkinter           as tk   # libreria per l'interfaccia grafica
 
 # Carica il dataframe dei dati dal file csv
 
-df_seriestoriche = pd.read_csv('NCFAdati.csv')
+df_sstor = pd.read_csv('NCFAdati.csv',sep=';')
 
+# Crea il dataframe con i totali di BDT
+
+df_totbdt = pd.DataFrame()
+df_totbdt['MESE'] = df_sstor['MESE']
+df_totbdt['FL_BDT'] = df_sstor['FL_IMP']+df_sstor['FL_PER']+df_sstor['FL_AZR']+df_sstor['FL_RET']
+df_totbdt['CP_BDT'] = df_sstor['CP_IMP']+df_sstor['CP_PER']+df_sstor['CP_AZR']+df_sstor['CP_RET']
+df_totbdt['ST_BDT'] = df_sstor['ST_IMP']+df_sstor['ST_PER']+df_sstor['ST_AZR']+df_sstor['ST_RET']
